8852e96350a11004cb2f09c00b3908130d4e8c7d
Rob Martin <rob@version2beta.com>
2016-03-05 05:44:39 -0700

    Stub API endpoints

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,7 +1,7 @@
 import amqp
 import logging, sys
 
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
@@ -14,6 +14,30 @@ def hello_world():
 def rabbit_test():
   amqp.publish_event(id='personid', event_type='test_event', event='another event message')
   return 'Tried to update Rabbit.'
+
+@app.route('/people', methods=['GET'])
+def get_people():
+  return 'Not implemented.'
+
+@app.route('/people', methods=['POST'])
+def add_person():
+  return 'Not implemented.'
+
+@app.route('/people/<id>', methods=['GET'])
+def get_person(id):
+  return 'Not implemented.'
+
+@app.route('/people/<id>', methods=['PUT'])
+def update_person(id):
+  return 'Not implemented.'
+
+@app.route('/people/<id>/incidents', methods=['GET'])
+def get_incidents(id):
+  return 'Not implemented.'
+
+@app.route('/people/<id>/incidents', methods=['POST'])
+def add_incident(id):
+  return 'Not implemented.'
 
 if __name__ == '__main__':
   app.run()
